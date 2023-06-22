1a5454ee9426b9ac837a147427b1f4b651a4c146
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-06-22 12:43:02 +0400

    feat:Настроить меню для мобильной версии

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,31 @@ function Header({ isLoggedIn, headerMenuData, email, signOut }) {
 
   const [sliderMenu, setSliderMenu] = React.useState(false);
 
+  function handleSignOut() {
+    setSliderMenu(false);
+    signOut();
+  }
+
   return(
     <header className="header">
       {!sliderMenu && <img src={logo} alt="Логотип Mesto" className="logo" />}
-      <div className={`header__menu ${sliderMenu && 'header__menu_opened'}`}>
+
+      <div className={`header__menu ${isLoggedIn && 'header__menu_hidden'} ${sliderMenu && 'header__menu_opened'}`}>
         {isLoggedIn && <p className="header__email">{email}</p>}
 
         <Link
           to={headerMenuData.link}
           className="header__sign-link"
-          onClick={isLoggedIn && signOut}
+          onClick={isLoggedIn && handleSignOut}
         >
           {headerMenuData.name}
         </Link>
       </div>
+
       {sliderMenu && <img src={logo} alt="Логотип Mesto" className="logo" />}
-      <div className="header__slider-menu" onClick={() => setSliderMenu(!sliderMenu)}>
+
+      {isLoggedIn &&
+        <div className="header__slider-menu" onClick={() => setSliderMenu(!sliderMenu)}>
         {!sliderMenu
           ? <div className="header__slider-open">
               <hr className="header__slider-line" />
@@ -29,7 +38,9 @@ function Header({ isLoggedIn, headerMenuData, email, signOut }) {
               <hr className="header__slider-line" />
             </div>
           : <div className="header__slider-close"></div>}
-      </div>
+        </div>
+      }
+
 
     </header>
   )
